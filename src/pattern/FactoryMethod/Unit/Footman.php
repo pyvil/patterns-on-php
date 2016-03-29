b6a8c390645ae87ccf9185133d5c68d86e3fed3a
vipya <vipya@smile.fr>
2016-03-30 02:29:44 +0300

    factory method in process

---
+++ src/pattern/FactoryMethod/Unit/Footman.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 30.03.16
+ * Time: 2:25
+ */
+
+namespace Pattern\FactoryMethod\Unit;
+
+use Pattern\FactoryMethod;
+
+
+class Footman implements FactoryMethod\UnitInterface
+{
+
+    /**
+     * @return mixed
+     */
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+}
---
+++ src/pattern/FactoryMethod/UnitInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 30.03.16
+ * Time: 2:13
+ */
+
+namespace Pattern\FactoryMethod;
+
+
+interface UnitInterface
+{
+
+}
